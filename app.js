e938bee8ee2a6313c3ba0c850b29e74e924510d0
3KINGZ <victorkenneth95@gmail.com>
2020-01-30 02:48:03 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+//variable declaration for dom elements
+let message;
+let dynamicCurl = "loading.com"
+let nameContainer = document.querySelector("#name");
+let imgContainer = document.querySelector("#image");
+let traits = document.querySelector("#traits");
+let personalities = document.querySelector("#personalities");
+let tweetContainer = document.querySelector("#tweet")
+
+//function that check the signs
+const checkSign = (date, month) => {
+    if (date >= 21 && month === "March" || date <= 19 && month === "April") {
+        return signs.aries;
+    } else if (date >= 20 && month === "April" || date <= 20 && month === "May") {
+        return signs.taurus;
+    } else if (date >= 21 && month === "May" || date <= 20 && month === "June") {
+        return signs.gemini;
+    } else if (date >= 21 && month === "June" || date <= 22 && month === "July") {
+        return signs.cancer;
+    } else if (date >= 23 && month === "July" || date <= 23 && month === "August") {
+        return signs.leo;
+    } else if (date >= 23 && month === "August" || date <= 22 && month === "September") {
+        return signs.virgo;
+    } else if (date >= 23 && month === "September" || date <= 22 && month === "October") {
+        return signs.libra;
+    } else if (date >= 23 && month === "October" || date <= 21 && month === "November") {
+        return signs.scorpio;
+    } else if (date >= 22 && month === "November" || date <= 21 && month === "December") {
+        return signs.sagittarius;
+    } else if (date >= 22 && month === "December" || date <= 19 && month === "January") {
+        return signs.capricorn;
+    } else if (date >= 20 && month === "January" || date <= 18 && month === "Febuary") {
+        return signs.aquarius;
+    } else if (date >= 19 && month === "Febuary" || date <= 20 && month === "March") {
+        return signs.pisces;
+    }
+}
+
+//function to setup the twitter button
+const setUpTweetBtn = (dynamicMessage, dynamicCurl) => {
+    let tweetButton = document.createElement("a");
+    tweetButton.textContent = "tweet";
+    tweetButton.setAttribute("href", "https://twitter.com/share?ref_src=twsrc%5Etfw");
+    tweetButton.setAttribute("class", "twitter-share-button");
+    tweetButton.setAttribute("data-size", "large")
+    tweetButton.setAttribute("data-hashtags", "HolidayCountDown")
+    tweetButton.setAttribute("data-text", dynamicMessage);
+    tweetButton.setAttribute('data-url', dynamicCurl)
+    tweetButton.setAttribute("data-show-count", "false")
+    document.querySelector("#tweet").appendChild(tweetButton)
+    twttr.widgets.load();
+}
+
+//function to render dom to the browser
+const renderDOM = (checker) => {
+    let sign = checker; //get the object value that is returned from the checkSign function for reference purposes
+    message = `I'm curious...What is your Zodiac sign? Mine is ${sign.name}`; //message value for the tweet function
+
+    //creating the name dom element that will display the name of the sign e.g Cancer
+    let name = document.createElement("h2");
+    name.textContent = sign.name;
+    nameContainer.appendChild(name);
+
+    //DOM element for the image
+    let image = document.createElement("img");
+    image.src = sign.image;
+    image.setAttribute("width", "60px");
+    imgContainer.appendChild(image);
+
+    //DOM element for traits and personalities that holds all the traits and personalities in each signs
+    let traitsUl = document.createElement("ul");
+    let personalitiesUl = document.createElement("ul");
+    sign.traits.forEach(trait => {
+        let li = document.createElement("li");
+        li.textContent = trait;
+        traitsUl.appendChild(li);
+    })
+
+    sign.personalities.forEach(personality => {
+        let li = document.createElement("li");
+        li.textContent = personality;
+        personalitiesUl.appendChild(li);
+    })
+
+    //DOM element for the header of traits and personalities
+    let titleTraits = document.createElement("h3");
+    titleTraits.textContent = "Traits";
+    titleTraits.setAttribute("class", "underline");
+    traits.appendChild(titleTraits);
+    traits.appendChild(traitsUl);
+
+    let titlePersonality = document.createElement("h3");
+    titlePersonality.textContent = "Personalities";
+    titlePersonality.setAttribute("class", "underline")
+    personalities.appendChild(titlePersonality);
+    personalities.appendChild(personalitiesUl);
+    //twitter DOM setup
+    setUpTweetBtn(message, dynamicCurl)
+}
+
+//event listener for the button where all the functions are called
+document.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    let date = document.querySelector("#date").value
+    let month = document.querySelector("#month").value
+    nameContainer.innerHTML = "";
+    imgContainer.innerHTML = "";
+    traits.innerHTML = "";
+    personalities.innerHTML = "";
+    tweetContainer.innerHTML = "";
+    renderDOM(checkSign(date, month));
+})
---
+++ sign-objects.js
@@ -0,0 +1,74 @@
+let signs = {
+    aries: {
+        name: "Aries",
+        image: "icon/002-aries.png",
+        traits: ["Adventurous and energetic", "Pioneering and courageois", "Enthusiastic and confident", "Dynamic and quick-witted"],
+        personalities: ["Selfish and quick-tempered", "Impulsive and impatient", "Foolhardy and daredevil"]
+    },
+    taurus: {
+        name: "Taurus",
+        image: "icon/022-taurus.png",
+        traits: ["Patient and reliable", "Warmhearted and loving", "Persistent and determined", "Placid and security loving"],
+        personalities: ["Jealous and possessive", "Resentful and inflexible", "Self-indulgent and greedy"]
+    },
+    gemini: {
+        name: "Gemini",
+        image: "icon/007-gemini.png",
+        traits: ["Adaptable and versatile", "Communicative and witty", "Intellectual and eloquent", "Youthful and lively"],
+        personalities: ["Nervous and tense", "Superficial and inconsistent", "Cunning and inquisitive"]
+    },
+    cancer: {
+        name: "Cancer",
+        image: "icon/003-cancer.png",
+        traits: ["Emotional and loving", "Intuitive and imaginative", "Shrewd and cautious", "Protective and sympathetic"],
+        personalities: ["Changeable and moody", "Overemotional and touchy", "Clinging and unable to let go"]
+    },
+    leo: {
+        name: "Cancer",
+        image: "icon/009-leo.png",
+        traits: ["Generous and warmhearted", "Creative and enthusiastic", "Broad-minded and expansive", "Faithful and loving"],
+        personalities: ["Pompous and patronizing", "Bossy and interfering", "Dogmatic and intolerant"]
+    },
+    virgo: {
+        name: "Virgo",
+        image: "icon/025-virgo.png",
+        traits: ["Modest and shy", "Meticulous and reliable", "Practical and diligent", "Intelligent and analytical"],
+        personalities: ["Fussy and a worrier", "Overcritical and harsh", "Perfectionist and conservative"]
+    },
+    libra: {
+        name: "Libra",
+        image: "icon/010-libra.png",
+        traits: ["Diplomatic and urbane", "Romantic and charming", "Easygoing and sociable", "Idealistic and peaceable"],
+        personalities: ["Indecisive and changeable", "Gullible and easily influenced", "Flirtatious and self-indulgent"]
+    },
+    scorpio: {
+        name: "Scorpio",
+        image: "icon/020-scorpio.png",
+        traits: ["Determined and forceful", "Emotional and intuitive", "Powerful and passionate", "Exciting and magnetic"],
+        personalities: ["Jealous and resentful", "Compulsive and obsessive", "Secretive and obstinate"]
+    },
+    sagittarius: {
+        name: "Sagittarius",
+        image: "icon/018-sagittarius.png",
+        traits: ["Optimistic and freedom-loving", "Jovial and good-humored", "Honest and straightforward", "Intellectual and philosophical"],
+        personalities: ["Blindly optimistic and careless", "Irresponsible and superficial", "Tactless and restless"]
+    },
+    capricorn: {
+        name: "Capricorn",
+        image: "icon/004-capricorn.png",
+        traits: ["Practical and prudent", "Ambitious and disciplined", "Patient and careful", "Humorous and reserved"],
+        personalities: ["Pessimistic and fatalistic", "Miserly and grudging"]
+    },
+    aquarius: {
+        name: "Aquarius",
+        image: "icon/001-aquarius.png",
+        traits: ["Friendly and humanitarian", "Honest and loyal", "Original and inventive", "Independent and intellectual"],
+        personalities: ["Intractable and contrary", "Perverse and unpredictable", "Unemotional and detached"]
+    },
+    pisces: {
+        name: "Pisces",
+        image: "icon/016-pisces.png",
+        traits: ["Imaginative and sensitive", "Compassionate and kind", "Selfless and unworldly", "Intuitive and sympathetic"],
+        personalities: ["Escapist and idealistic", "Secretive and vague", "Weak-willed and easily led"]
+    }
+}
